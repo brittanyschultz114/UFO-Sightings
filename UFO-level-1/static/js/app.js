e514cfc7b34e5678a525c2ca36e221ed80a5a08f
unknown <brittanyschultz114@gmail.com>
2021-03-21 15:05:40 -0400

    added to app file

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,52 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// console.log(tableData)
+
+
+// Select the button
+var button = d3.select("#filter-btn")
+
+// Select the form
+var form = d3.select("form")
+
+// Create event handlers 
+button.on("click", runEnter)
+form.on("submit",runEnter)
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+    d3.event.preventDefault()
+
+  // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime")
+
+  // Get the value property of the input element
+    var inputValue = inputElement.property("value")
+
+    // console.log(inputValue)
+    // console.log(tableData)
+
+    // Filter data based on datetime
+    var filteredData = tableData.filter(date => date.datetime === inputValue)
+
+    // console.log(filteredData)
+
+    // Getting the body tag for the table
+    var tableBody = d3.select("tbody")
+
+    // Build the table in the html
+    tableBody.html("")
+
+    // Show filter data table
+    filteredData.forEach(function(sighting) {
+        var row = tableBody.append("tr")
+        Object.entries(sighting).forEach(function([key, value]) {
+            var cell = row.append("td")
+            cell.text(value)
+        })
+    })
+
+}
